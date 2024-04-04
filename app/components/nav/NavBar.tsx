c8c68b3b379f4843025d05cfa5b4975b5c70f505
Olubukola-Martins <19bukola97@gmail.com>
2024-04-04 13:09:33 +0100

    antd installed

---
+++ app/components/nav/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image';
+
+
+const NavBar = () => {
+  return (
+    // #557FF2
+    <div><Image alt="logo" src="/ic_logo.png" width={92} height={29}/> </div>
+  )
+}
+
+export default NavBar
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import NavBar from "./components/nav/NavBar";
 
 export default function Home() {
   return (
- <div className="mx-auto w-[86.11%] border border-stone-800">Area Finder</div> );
+ <div className="mx-auto w-[86.11%] ">
+  <NavBar/>
+
+ </div> );
 }
